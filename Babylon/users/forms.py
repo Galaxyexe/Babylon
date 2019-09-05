f94c9b4ceb28c3abe68778271e791d36aa482733
Yoshterra1 <44651405+Yoshterra1@users.noreply.github.com>
2019-09-04 20:29:58 -0400

    Beggining steps of improved user templates created

--- Babylon/users/forms.py
+++ Babylon/users/forms.py
@@ -1,8 +1,8 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Submit
+#from crispy_forms.helper import FormHelper
+#from crispy_forms.layout import Submit
 #from .models import Profile
 
 
@@ -14,10 +14,12 @@ class UserRegisterForm(UserCreationForm):
         model = User
         fields = ['username', 'email', 'number', 'password1', 'password2']
 
-    def __init__(self, *args, **kwargs):
+    #to be replaced with regular forms
+    '''def __init__(self, *args, **kwargs):
         super(UserRegisterForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper(self)
         self.helper.field_template = 'bootstrap3/layout/inline_field.html'
         self.helper.add_input(
             Submit('submit', 'Submit', css_class='btn-primary'))
         self.helper.form_method = 'POST'
+        '''
