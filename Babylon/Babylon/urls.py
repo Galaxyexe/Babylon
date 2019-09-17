869cec1f30d940889bd655c307e997d053273a71
James R <jamesriordan39@gmail.com>
2019-09-17 18:08:23 -0400

    Game page and profile work

--- Babylon/Babylon/urls.py
+++ Babylon/Babylon/urls.py
@@ -19,7 +19,7 @@ urlpatterns = [
     path('waves/', general_views.waves, name="waves"),
     path('register/', user_views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name="users/login.html"), name="login"),
-    path('logout/', general_views.home, name="logout"),
+    path('logout/', auth_views.LogoutView.as_view(template_name="general/index.html"), name="logout"),
     path('profile/', user_views.profile, name="profile"),
     # path('register/', user_views.register, name='register'),
     # path('login/', auth_views.LoginView.as_view(template_name="users/login.html"), name="login"),
